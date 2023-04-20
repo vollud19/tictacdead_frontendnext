7311874673442b5b99daade148cb35a93ccbac16
vollud19 <vollud19@htl-kaindorf.at>
2023-04-20 16:38:27 +0200

    Added functionality to buttons

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -29,11 +29,11 @@ const Navbar = () => {
     return (
         <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
             <div>{screenWidth}</div>
-            <h1 className='w-full font-redundead md:text-4xl sm:text-2xl text-2xl font-bold text-yellow-700'>TIC·TAC·<span className='text-red-950'>Dead</span></h1>
+            <h1 onClick={() => router.push("/")} className='w-full font-redundead md:text-4xl sm:text-2xl text-2xl font-bold text-yellow-700'>TIC·TAC·<span className='text-red-950'>Dead</span></h1>
             <ul className='hidden md:flex'> {/*}when collapsing the navbar on the top which would also collapse hides*/}
-                <li className='p-4'><a><Link to="hero" spy={true} smooth={true} offset={50} duration={200}>Home</Link></a></li>
-                <li className='p-4'><a><Link to="description" spy={true} smooth={true} offset={50} duration={200}>Description</Link></a></li>
-                <li className='p-4'><a><Link to="about" spy={true} smooth={true} offset={50} duration={200}>About</Link></a></li>
+                <li className='p-4'><button onClick={() => router.push("/")}>Home</button></li>
+                <li className='p-4'><button><Link to="description" spy={true} smooth={true} offset={50} duration={200}>Description</Link></button></li>
+                <li className='p-4'><button><Link to="about" spy={true} smooth={true} offset={50} duration={200}>About</Link></button></li>
                 <li className='p-2'>
                     <button onClick={() => router.push("/playgame")}
                         className="whitespace-nowrap break-keep bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-10 border border-gray-400 rounded shadow">
@@ -44,13 +44,13 @@ const Navbar = () => {
                 {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
             </div>
             <div className={
-                !nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500': 'fixed left-[-100%]'}>
+                !nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000100] ease-in-out duration-500': 'fixed left-[-100%]'}>
                 <h1 className='w-full font-redundead md:text-4xl sm:text-2xl text-2xl font-bold text-yellow-700 m-4'>TIC·TAC·<span className='text-red-950'>Dead</span></h1>
                 <ul className='uppercase p-4'>
-                    <li className='p-4 border-b border-gray-600'><a><Link to="home" spy={true} smooth={true} offset={50} duration={2}>Home</Link></a></li>
-                    <li className='p-4 border-b border-gray-600'><a><Link to="description" spy={true} smooth={true} offset={50} duration={200}>Description</Link></a></li>
-                    <li className='p-4 border-b border-gray-600'><a><Link to="about" spy={true} smooth={true} offset={50} duration={200}>About</Link></a></li>
-                    <li className='p-4'>Play</li>
+                    <li className='p-4 border-b border-gray-600'><button><Link to="home" spy={true} smooth={true} offset={50} duration={2}>Home</Link></button></li>
+                    <li className='p-4 border-b border-gray-600'><button><Link to="description" spy={true} smooth={true} offset={50} duration={200}>Description</Link></button></li>
+                    <li className='p-4 border-b border-gray-600'><button><Link to="about" spy={true} smooth={true} offset={50} duration={200}>About</Link></button></li>
+                    <li className='p-4'><button onClick={() => router.push("playgame")}>PLAY</button></li>
                 </ul>
             </div>
         </div>
